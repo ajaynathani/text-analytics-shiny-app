513c6cdfe0c11971922a42aeb9f67d3b6133c23a
ajaynathani <ajay.nathani@yahoo.com>
2019-12-29 22:55:32 +0530

    Add files via upload

---
+++ dependecy-packages.R
@@ -0,0 +1,15 @@
+
+
+try(require("shiny")||install.packages("shiny"))
+try(require("ggplot2")||install.packages("ggplot2"))
+try(require("magrittr")||install.packages("magrittr"))
+try(require("tidytext")||install.packages("tidytext"))
+try(require("tidyverse")||install.packages("tidyverse"))
+try(require("wordcloud")||install.packages("wordcloud"))
+
+library(shiny)
+library(ggplot2)
+library(magrittr)
+library(tidytext)
+library(tidyverse)
+library(wordcloud)
